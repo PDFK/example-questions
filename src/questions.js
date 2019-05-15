33fd00bee312654bebfeccc1ea9ed28f0a41d826
PDFK <pedrof_42@yahoo.es>
2019-05-15 15:12:44 -0400

    [first commit]

---
+++ src/questions.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class Questions extends Component {
+  render () {
+    return (
+      <div>HOLA MUNDO</div>
+    )
+  }
+}
+
+export default Questions;
